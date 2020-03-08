bf8c08b68b86d81fa360d510a0c50191cb72763f
Adolfo <adolffoo@ciencias.unam.mx>
2020-03-08 16:52:57 -0600

    avance api

---
+++ springboot-servicio-productos/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Panasonic', 800, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Sony', 700, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Apple', 1000, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Adidas', 1100, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Nike', 1600, NOW());
